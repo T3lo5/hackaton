354c68388dbe153630274770a5dc3b1e3e4eb1c3
Anderson Franciscato <andersonwsf@gmail.com>
2023-08-20 13:06:04 -0300

    initial commit

---
+++ config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    database: {
+      host: 'localhost',
+      user: 'root',
+      password: '',
+      database: 'hackaton-db',
+      waitForConnections: true,
+      connectionLimit: 10,
+      queueLimit: 0
+    },
+    jwtSecret: '$smY6LFBs!mq2^' 
+  };
+  
